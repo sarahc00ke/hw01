673edc51dd42f925d49ca76abfb036bd58180eaa
Vivek Srikrishnan <vs498@cornell.edu>
2022-09-01 21:38:09 -0400

    remove pandoc

--- compile_report.jl
+++ compile_report.jl
@@ -6,13 +6,11 @@ using Weave
 
 function compile_report(fname, out_type = "pdf")
     if out_type == "pdf"
-        Weave.weave(fname, doctype="pandoc2pdf", template="bee4750.tpl", fig_path="figures")
+        Weave.weave(fname, doctype="md2pdf", template="bee4750pdf.tpl", fig_path="figures")
     elseif out_type == "html"
-        Weave.weave(fname, doctype="md2html", fig_path="figures")
-    elseif out_type == "html-pandoc"
-        Weave.weave(fname, doctype="pandoc2html", template="bee4750.tpl", fig_path="figures")
+        Weave.weave(fname, doctype="md2html", template="bee4750html.tpl", fig_path="figures")
     else
-        throw(ArgumentError("out_type must be pdf, html, or html-pandoc"))
+        throw(ArgumentError("out_type must be pdf, html"))
     end 
 end
 
